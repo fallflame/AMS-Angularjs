51fde142f052285bc18fede942f215d78bd5363c
Yan Xu <xuyantc@msn.com>
2015-05-15 22:20:06 -0400

    add link to backup

--- routes/api.js
+++ routes/api.js
@@ -256,8 +256,6 @@ router.get('/backup_db', function(req, res){
 
 	child = exec('mysqldump ams > ' + filePath,
 		function (error, stdout, stderr) {
-		console.log('stdout: ' + stdout);
-		console.log('stderr: ' + stderr);
 		if (error !== null) {
 		console.log('exec error: ' + error);
 		}
